18528fd809973fb628a4573c9261158e9d8a2b73
AtiM07 <atim203011@gmail.com>
2023-10-12 11:37:34 +0300

    Добавлена сцена боя

---
+++ Assets/Scripts/BattleLoader.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BattleLoader : MonoBehaviour
+{
+    [SerializeField] private Slider sliderLeft;
+    [SerializeField] private Slider sliderRight;
+    [SerializeField] private GameObject bangImage;
+    [SerializeField] private GameObject okPopUpWindow;
+
+    [SerializeField] private float loadTime = 0.0015f;
+
+    void Start()
+    {
+        sliderLeft.value = 0;
+        sliderRight.value = 0;
+        bangImage.SetActive(false);
+        StartCoroutine(Loading());
+    }
+
+    IEnumerator Loading()
+    {
+        var time = loadTime;
+        var value = 0f;
+        while (value < 1)
+        {
+            yield return new WaitForSecondsRealtime(time);
+            value += time;
+            sliderLeft.value = value;
+            sliderRight.value = value;
+        }
+
+        bangImage.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        OpenWindow();
+    }
+
+    private void OpenWindow()
+    {
+        var script = okPopUpWindow.GetComponent<OkPopUpUI>();
+        script.SetTitleText("Результат боя:");
+        script.SetResultText("Бой прошел");
+        var okObject = Instantiate(okPopUpWindow, transform.parent);
+        script = okObject.GetComponent<OkPopUpUI>();
+        script.okButton.onClick.AddListener(() =>
+            {
+                Debug.Log("BANG");
+                SceneManager.LoadScene("MainScene");
+            }
+        );
+    }
+}
--- Assets/Scripts/PopUp/OkPopUpUI.cs
+++ Assets/Scripts/PopUp/OkPopUpUI.cs
@@ -4,16 +4,24 @@ using UnityEngine.UI;
 
 public class OkPopUpUI : MonoBehaviour
 {
-    [SerializeField] private Button okButton;
+    [SerializeField] public Button okButton;
+    [SerializeField] private TextMeshProUGUI resultText;
     [SerializeField] private TextMeshProUGUI titleText;
 
     private void Start()
     {
         okButton.onClick.AddListener(() => Destroy(gameObject));
+        resultText.text = "0";
+        titleText.text = "Результат выполнения:";
     }
 
     public void SetResultText(string result)
     {
-        titleText.text = result;
+        resultText.text = result;
+    }
+
+    public void SetTitleText(string title)
+    {
+        titleText.text = title;
     }
 }
